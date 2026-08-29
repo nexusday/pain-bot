dfbc07673fd5afa38bd1a7131828837785932125
nexusday <nexusdaynex@gmail.com>
2026-08-29 14:21:29 -0500

    subbots

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ ch1: '120363405059433072@newsletter',
 
 // Canal para logs/anuncios de sub-bots (nuevo sub-bot, etc.). NO es global.canal
 // Obtén el JID con: .canalid <link>  |  Ejemplo: '120363405059433072@newsletter'
-global.logssubbots = ''
+global.logssubbots = '120363430965244922@newsletter'
 
 global.mods =   []
 global.prems = []
